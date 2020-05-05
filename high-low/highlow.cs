3295f8d42c86075ddd0c3c494c19d5207de9bc61
Christopher <CHuber42.work@gmail.com>
2020-05-05 12:48:05 -0700

    add high-low game and .gitignore entries (correctly)

--- high-low/highlow.cs
+++ high-low/highlow.cs
@@ -1,8 +1,29 @@
 using System;
 
-class Car{
+class highlow{
     static void Main()
     {
-        
+        Random rand = new Random();
+        int winNumber = rand.Next(100);
+        int userGuess = -1;
+        Console.WriteLine("I am thinking of a number between 1 and 100. Please choose a number and I will tell you if my number is higher or lower than that.");
+
+        while (userGuess != winNumber)
+        {
+            string userGuessInput = Console.ReadLine();
+            userGuess = int.Parse(userGuessInput);
+
+            if (userGuess > winNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < winNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("Correct! You Win!");
+            }
+        }
     }
 }
